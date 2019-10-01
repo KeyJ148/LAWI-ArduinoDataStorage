55dbb69039f6020dddb0849c29d64081d3e31612
Евгений Серов <kejik148@gmail.com>
2019-10-01 15:41:16 +0300

    В Package добавлена проверка на длину поступающего массива и метод toString()

--- src/lawi/ads/arduino/Package.java
+++ src/lawi/ads/arduino/Package.java
@@ -6,15 +6,31 @@ import java.util.Arrays;
 
 public class Package {
 
+    public static final int LENGTH = 13;
+
     public float t;
     public float h;
     public float p;
     public byte bmeStatus;
 
     public Package(byte[] data){
+        if (data.length < 13) throw new IllegalArgumentException("Length data must be equal or greater than " + LENGTH + " (Package.LENGTH)");
+
         t = ArrayUtils.byteArrayToFloat(Arrays.copyOfRange(data, 0, 4));
         h = ArrayUtils.byteArrayToFloat(Arrays.copyOfRange(data, 4, 8));
         p = ArrayUtils.byteArrayToFloat(Arrays.copyOfRange(data, 8, 12));
         bmeStatus = data[12];
+    }
+
+    @Override
+    public String toString(){
+        StringBuilder sb = new StringBuilder();
+
+        sb.append("Temperature: "); sb.append(t); sb.append(" *C\n");
+        sb.append("Humidity: "); sb.append(h); sb.append(" %\n");
+        sb.append("Pressure: "); sb.append(p); sb.append(" mm Hg\n");
+        sb.append("BME status: "); sb.append(bmeStatus); sb.append("\n");
+
+        return sb.toString();
     }
 }
