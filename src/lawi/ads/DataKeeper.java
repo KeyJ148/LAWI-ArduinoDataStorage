9b51c60455358e77c25907076a0ed450ddb2ccb4
Евгений Серов <kejik148@gmail.com>
2019-10-03 21:01:52 +0300

    В DataKeeper добавлена возможность отключения arduino

--- src/lawi/ads/DataKeeper.java
+++ src/lawi/ads/DataKeeper.java
@@ -6,25 +6,40 @@ import lawi.ads.arduino.Package;
 public class DataKeeper implements Runnable{
 
     private Arduino arduino;
-    private boolean work = true;
+    private boolean work = false, stop = false;
 
     public DataKeeper(Arduino arduino){
         this.arduino = arduino;
-        new Thread(this).start();
+
+        Thread thread = new Thread(this);
+        thread.setDaemon(false);
+        thread.setName("DataKeeper");
+        thread.start();
+    }
+
+    public void stop(){
+        stop = true;
+        arduino.closeConnection();
     }
 
     @Override
     public void run(){
         try {
             arduino.openConnection();
-            while (true){
+            work = true;
+            while (!stop){
                 Package pack = arduino.nextPackage();
                 System.out.println(pack); //TODO: Лог и запись в БД
             }
         } catch (Exception e){
-            work = false;
-
             e.printStackTrace(); //TODO: Лог и вывод на консоль/сокет
+        } finally {
+            try {
+                arduino.closeConnection();
+                work = false;
+            } catch (Exception e){
+                e.printStackTrace(); //TODO: Лог и вывод на консоль/сокет
+            }
         }
     }
 
