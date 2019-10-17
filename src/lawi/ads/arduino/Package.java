a080cb0de0a0a63e7123e8ff5f2c142b03c15f4e
Евгений Серов <kejik148@gmail.com>
2019-10-17 19:19:57 +0300

    Добавлена обработка данных с датчика MHZ19b

--- src/lawi/ads/arduino/Package.java
+++ src/lawi/ads/arduino/Package.java
@@ -6,20 +6,24 @@ import java.util.Arrays;
 
 public class Package {
 
-    public static final int LENGTH = 13;
+    public static final int LENGTH = 18;
 
     public float t;
     public float h;
     public float p;
+    public int co2;
     public byte bmeStatus;
+    public byte mhzStatus;
 
     public Package(byte[] data){
-        if (data.length < 13) throw new IllegalArgumentException("Length data must be equal or greater than " + LENGTH + " (Package.LENGTH)");
+        if (data.length < LENGTH) throw new IllegalArgumentException("Length data must be equal or greater than " + LENGTH + " (Package.LENGTH)");
 
         t = ArrayUtils.byteArrayToFloat(Arrays.copyOfRange(data, 0, 4));
         h = ArrayUtils.byteArrayToFloat(Arrays.copyOfRange(data, 4, 8));
         p = ArrayUtils.byteArrayToFloat(Arrays.copyOfRange(data, 8, 12));
-        bmeStatus = data[12];
+        co2 = ArrayUtils.byteArrayToInt(Arrays.copyOfRange(data, 12, 16));
+        bmeStatus = data[16];
+        mhzStatus = data[17];
     }
 
     @Override
@@ -29,7 +33,9 @@ public class Package {
         sb.append("Temperature: "); sb.append(t); sb.append(" *C\n");
         sb.append("Humidity: "); sb.append(h); sb.append(" %\n");
         sb.append("Pressure: "); sb.append(p); sb.append(" mm Hg\n");
+        sb.append("CO2: "); sb.append(co2); sb.append(" ppm\n");
         sb.append("BME status: "); sb.append(bmeStatus); sb.append("\n");
+        sb.append("MHZ status: "); sb.append(mhzStatus); sb.append("\n");
 
         return sb.toString();
     }
